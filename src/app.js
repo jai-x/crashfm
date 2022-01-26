1bb8a6aa92f262ab467f273a21335a495c289a42
Jai Sharma <jai@jai.moe>
2022-01-26 01:05:22 +0000

    Rebuild using mithril

--- src/app.js
+++ src/app.js
@@ -1,187 +1,244 @@
-import { gsap } from 'gsap';
+import m from 'mithril/hyperscript';
+import mount from 'mithril/mount';
+import redraw from 'mithril/redraw';
+import gsap from 'gsap/index';
+
 import stations from './stations';
-// import motds from "./motds.js";
 import motds from './motds_emoji';
 
-const select = (str) => document.querySelector(str);
-const selectAll = (str) => document.querySelectorAll(str);
-
-const elemTrackStatic = select('#track-static');
-const elemTrackMusic = select('#track-music');
-const elemSplashLoading = select('.splash-loading');
-const elemSplashStart = select('.splash-start');
-const elemNextButton = select('.next-button');
-const elemNowPlaying = select('.now-playing');
-const elemProgressBar = select('.progress-bar');
-const elemLogo = select('.logo');
-const elemBackground = select('.background');
-const elemTickerBody = select('.ticker');
-const elemTickerMove = select('.ticker-move');
-const elemTickerRows = selectAll('.ticker-row');
-
-let tracks = [];
-let unplayed = [];
-let nowPlaying = null;
-let progressInterval = null;
-let loading = false;
-let tickerAnim = null;
-
-const loadMotds = (messages) => {
-  const makeDiv = (cssClass, text) => {
-    const e = document.createElement('div');
-    e.classList.add(cssClass);
-    const txt = document.createTextNode(text);
-    e.appendChild(txt);
-    return e;
-  };
-
-  const shuffled = gsap.utils.shuffle(new Array(...messages));
-
-  const elems0 = shuffled.reduce((e, m) => {
-    e.push(makeDiv('ticker-message', m));
-    e.push(makeDiv('ticker-gap', '•'));
-    return e;
-  }, []);
-  const elems1 = elems0.map((e) => e.cloneNode(true));
-
-  elemTickerRows[0].append(...elems0);
-  elemTickerRows[1].append(...elems1);
-
-  const widthTicker = elemTickerBody.offsetWidth;
-  const widthRow = elemTickerRows[0].offsetWidth;
-
-  gsap.set(elemTickerMove, { x: widthTicker });
-
-  const speed = 225; // px/second ?
-
-  const durations = {
-    init: widthTicker / speed,
-    loop: widthRow / speed,
-  };
-
-  const tl = gsap.timeline({ paused: true });
-
-  tl.to(elemTickerMove, {
-    duration: durations.init,
-    ease: 'linear',
-    x: 0,
-  });
-  tl.to(elemTickerMove, {
-    duration: durations.loop,
-    ease: 'linear',
-    x: -widthRow,
-    repeat: -1,
-  });
-
-  tickerAnim = tl;
+const shuffle = (array) => {
+  const copy = new Array(...array);
+  return copy.sort((a, b) => 0.5 - Math.random());
 };
 
-const updateImgSrc = (imgElem, imgSrc) => {
-  const tl = gsap.timeline({ paused: true });
+class CrashFM {
+  station;
+  started;
+  loading;
+  np;
 
-  tl.to(imgElem, { opacity: 0, duration: 0.25 });
-  tl.set(imgElem, { src: imgSrc });
-  tl.to(imgElem, { opacity: 1, duration: 0.25 });
-  tl.set(imgElem, { clearProps: 'all' });
+  constructor(stations) {
+    this.stations = stations;
+    this.station = {};
+    this.started = false;
+    this.loading = true;
+    this.np = {};
+    this.unplayed = [];
 
-  tl.play();
-};
+    this.trackMusic = new Audio();
+    this.trackMusic.autoplay = false;
+    this.trackMusic.addEventListener('ended', this.loadNextTrack.bind(this));
 
-const loadStation = (name) => {
-  const station = stations[name];
-
-  updateImgSrc(elemLogo, station.logo);
-  updateImgSrc(elemBackground, station.background);
-
-  unplayed = [];
-  tracks = station.tracks;
-};
-
-const startProgress = () => {
-  if (progressInterval) {
-    return;
+    this.trackStatic = new Audio('/static/static.mp3');
+    this.trackStatic.autoplay = false;
+    this.trackStatic.loop = true;
   }
 
-  const updateProgress = (bar, track) => {
-    const percent = `${((track.currentTime / track.duration) * 100).toFixed(3)}%`;
-    gsap.set(bar, { width: percent});
-  };
+  start() {
+    console.log(`${this.constructor.name}: start`);
 
-  progressInterval = setInterval(() => {
-    updateProgress(elemProgressBar, elemTrackMusic);
-  }, 150);
-};
-
-const stopProgress = () => {
-  if (!progressInterval) {
-    return;
+    if (!this.station) {
+      throw 'loadStation must be called first';
+    }
+    this.started = true;
+    this.loadNextTrack();
   }
 
-  clearInterval(progressInterval);
-  progressInterval = null;
-  gsap.set(elemProgressBar, { clearProps: 'all' });
-};
+  loadStation(name) {
+    console.log(`${this.constructor.name}: loadStation`);
 
-const setLoading = () => {
-  loading = true;
-  gsap.set(elemNextButton, { opacity: 0.5, cursor: 'not-allowed' });
-};
+    const newStation = this.stations[name];
+    if(this.station === newStation) {
+      return;
+    }
 
-const stopLoading = () => {
-  gsap.set(elemNextButton, { clearProps: 'all' });
-  loading = false;
-};
-
-const trackNext = () => {
-  if (loading) {
-    return;
+    this.station = newStation;
+    this.unplayed = [];
   }
 
-  setLoading();
-  elemTrackMusic.pause();
-  stopProgress();
-  elemTrackStatic.play();
+  loadNextTrack() {
+    console.log(`${this.constructor.name}: loadNextTrack`);
 
-  elemNowPlaying.innerText = 'Loading...';
+    this.loading = true;
+    this.trackMusic.pause();
+    this.trackStatic.play();
 
-  if (unplayed.length === 0) {
-    unplayed = new Array(...tracks);
-    unplayed = gsap.utils.shuffle(unplayed);
+    if (this.unplayed.length === 0) {
+      this.unplayed = shuffle(this.station.tracks);
+    }
+    this.np = this.unplayed.pop();
+
+    this.trackMusic.addEventListener('canplay', () => {
+      setTimeout(this.startNextTrack.bind(this), 500);
+    }, { once: true })
+    this.trackMusic.src = this.np.path;
+    this.trackMusic.load();
   }
 
-  nowPlaying = unplayed.pop();
+  startNextTrack() {
+    console.log(`${this.constructor.name}: startNextTrack`);
 
-  elemTrackMusic.src = nowPlaying.path;
+    this.trackStatic.pause();
+    this.trackMusic.play();
+    this.loading = false;
+    redraw();
+  }
+
+  get progress() {
+    if (this.loading) {
+      return 0;
+    }
+    return ((this.trackMusic.currentTime / this.trackMusic.duration) * 100).toFixed(2);
+  }
 };
 
-const trackPlay = () => {
-  elemNowPlaying.innerText = `${nowPlaying.artist} - ${nowPlaying.title}`;
-  elemTrackStatic.pause();
-  elemTrackMusic.play();
-  startProgress();
-  setTimeout(stopLoading, 250); // prevents button spamming
+class Background {
+  view(vnode) {
+    return m('img.background', { src: vnode.attrs.background });
+  }
+
+  oncreate(vnode) {
+    gsap.fromTo(vnode.dom, { opacity: 0 }, { opacity: 1, duration: 0.25 });
+  }
+
+  onbeforeremove(vnode) {
+    return gsap.to(vnode.dom, { opacity: 0, duration: 0.25 }).then();
+  }
 };
 
-const start = () => {
-  gsap.to(elemSplashStart, {
-    opacity: 0,
-    duration: 0.5,
-    onComplete: () => { elemSplashStart.remove(); },
-  });
-  trackNext();
-  tickerAnim.play();
+class Logo {
+  view(vnode) {
+    return m('.logo-container', [
+      m('img.logo', { src: vnode.attrs.logo }),
+    ]);
+  }
+
+  oncreate(vnode) {
+    gsap.fromTo(vnode.dom, { opacity: 0 }, { opacity: 1, duration: 0.25 });
+  }
+
+  onbeforeremove(vnode) {
+    return gsap.to(vnode.dom, { opacity: 0, duration: 0.25 }).then();
+  }
+}
+
+class Splash {
+  view(vnode) {
+    return m('.splash', { onclick: () => cfm.start() }, [
+      m('span', 'Click anywhere to begin'),
+    ]);
+  }
+
+  onbeforeremove(vnode) {
+    return gsap.to(vnode.dom, { opacity: 0, duration: 1 }).then();
+  }
+}
+
+class Info {
+  view(vnode) {
+    const { np, loading } = vnode.attrs;
+    const text = (loading ? 'Loading...' : `${np.artist} - ${np.title}`);
+    const buttonClass = `next-button ${(loading ? 'next-button-loading' : '')}`;
+
+    return m('.info', [
+      m('.now-playing', text),
+      m('img',  {
+        class: buttonClass,
+        src: '/static/next.png',
+        onclick: () => cfm.loadNextTrack(),
+      }),
+    ]);
+  }
+}
+
+class Progress {
+  view(vnode) {
+    const { loading, progress } = vnode.attrs;
+
+    if (!loading) {
+      setTimeout(m.redraw, 200);
+    }
+
+    return m('.progress', [
+      m('.progress-bar', { style: { width: `${vnode.attrs.progress}%` }}),
+    ]);
+  }
+}
+
+class TickerRow {
+  view(vnode) {
+    const messages = vnode.attrs.messages;
+
+    return m('.ticker-row', ...messages.map((message) => {
+      return [ m('.ticker-message', message), m('.ticker-gap', '•') ];
+    }));
+  }
+}
+
+class Ticker {
+  oninit() {
+    this.moving = false;
+  }
+
+  view(vnode) {
+    const messages = vnode.attrs.messages;
+    return m('.ticker', [
+      m('.ticker-move', [
+        m(TickerRow, { messages }),
+        m(TickerRow, { messages }),
+      ]),
+    ]);
+  }
+
+  onupdate(vnode) {
+    if (!this.moving && vnode.attrs.started) {
+      this.animation.play();
+    }
+  }
+
+  oncreate(vnode) {
+    const speed = 225; // pixels/second ???
+    const frame = vnode.dom;
+    const frameWidth = frame.offsetWidth;
+    const move = vnode.dom.firstChild;
+    const rowWidth = move.firstChild.offsetWidth;
+
+    gsap.set(move, { x: frameWidth });
+
+    const tl = gsap.timeline({ paused: true });
+    tl.to(move, { x: 0, duration: frameWidth / speed, ease: 'linear' });
+    tl.to(move, { x: -rowWidth, duration: rowWidth / speed, ease: 'linear', repeat: -1 });
+    this.animation = tl;
+  }
+}
+
+class Main {
+  view(vnode) {
+    const { started, loading, np, progress, station: { background, logo } } = vnode.attrs.cfm;
+    const messages = vnode.attrs.messages;
+
+    return m('main', [
+      [
+        m(Background, { background, key: background }),
+      ],
+      m('.content', [
+        m('.selector', [
+          m(Logo, { logo, key: logo }),
+        ]),
+        m(Progress, { loading, progress }),
+        m(Info, { np, loading }),
+        m(Ticker, { messages, started }),
+      ]),
+      (!started ? m(Splash) : ''),
+    ]);
+  }
 };
 
-const setup = () => {
-  elemTrackMusic.addEventListener('canplay', () => { setTimeout(trackPlay, 1000); });
-  elemTrackMusic.addEventListener('ended', trackNext);
-  elemSplashStart.addEventListener('click', start);
-  elemNextButton.addEventListener('click', trackNext);
+const cfm = new CrashFM(stations);
+const messages = shuffle(motds);
 
-  loadMotds(motds);
-  loadStation('burnout3'); // default station
+cfm.loadStation('burnout3');
 
-  elemSplashLoading.remove();
-};
-
-setup();
+document.addEventListener('DOMContentLoaded', () => {
+  mount(document.body, { view: () => m(Main, { cfm, messages }) });
+});
